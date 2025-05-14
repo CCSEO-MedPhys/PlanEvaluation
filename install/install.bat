b3459bb54615516f6ddbf8f1037cdbb17a3c2e41
Greg Salomons <smokey.salomons@gmail.com>
2025-05-14 09:38:41 -0400

    cleaned folders

--- install/install.bat
+++
@@ -1,5 +0,0 @@
-Miniconda3-latest-Windows-x86_64.exe /s /InstallationType=AllUsers /RegisterPython=1 /D=C:\ProgramData\Miniconda3
-C:\ProgramData\Miniconda3\Scripts\activate.bat C:\ProgramData\Miniconda3
-conda config --append channels conda-forge
-conda create --name PlanEvaluation --file PlanEvaluation_spec.txt
-
