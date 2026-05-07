39a57d35d554edb67852a5fb7ed10441dcc3bfbf
GregSal <33795111+GregSal@users.noreply.github.com>
2026-05-07 14:51:38 -0400

    Added Virtual Environment for Deployment

---
+++ For Distribution/PlanEvaluation/PlanEvaluation.bat
@@ -0,0 +1,4 @@
+rem This must be executed in the PlanEvaluation Folder to work
+rem create a shortcut to this file and move the shortcut to the desired location
+CALL ".\PlanEvaluationVenv\Scripts\python.exe" ".\PlanEvaluation.py"
+
